8400612ae5dd1f4bf206c4463b04149c11f3d1df
Anastasia <stasya@MacBook-Air-Anastasia.local>
2023-11-12 17:09:27 +0300

    Обновление с правками

--- Task 2.3.sql
+++ Task 2.3.sql
@@ -1,20 +1,29 @@
 -- Задание 1: Возвращает список клиентов (имя и фамилия) с наибольшей общей суммой заказов
 SELECT c.FirstName, c.LastName
 FROM Customers c
-JOIN Orders o ON c.CustomerID = o.CustomerID
-GROUP BY c.FirstName, c.LastName
-ORDER BY SUM(o.TotalAmount) DESC
-LIMIT 1;
+JOIN (
+  SELECT o.CustomerID, SUM(o.TotalAmount) AS TotalAmount
+  FROM Orders o
+  GROUP BY o.CustomerID
+) t ON c.CustomerID = t.CustomerID
+WHERE t.TotalAmount = (
+  SELECT MAX(TotalAmount)
+  FROM (
+    SELECT SUM(o.TotalAmount) AS TotalAmount
+    FROM Orders o
+    GROUP BY o.CustomerID
+  ) t
+)
 
 -- Задание 2: Для каждого клиента из пункта 1 выводит список его заказов (номер заказа и общая сумма) в порядке убывания общей суммы заказов
+
 SELECT o.OrderID, o.TotalAmount
 FROM Orders o
 JOIN Customers c ON c.CustomerID = o.CustomerID
-WHERE c.CustomerID = (
-  SELECT c.CustomerID
-  FROM Customers c
-  JOIN Orders o ON c.CustomerID = o.CustomerID
-  GROUP BY c.FirstName, c.LastName
+WHERE c.CustomerID IN (
+  SELECT o.CustomerID
+  FROM Orders o
+  GROUP BY o.CustomerID
   ORDER BY SUM(o.TotalAmount) DESC
   LIMIT 1
 )
